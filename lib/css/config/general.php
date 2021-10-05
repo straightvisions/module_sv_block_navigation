ed77a3ccde14401f2df1ed5ccd5d7c1c5bdf194b
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-10-05 20:09:04 +0200

    ADD GUTENBERG DEFAULT STYLES

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-navigation' : '.sv100_sv_content_wrapper article .wp-block-navigation',
+		is_admin() ? '.editor-styles-wrapper .wp-block-navigation' : '.wp-block-navigation',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
@@ -9,7 +9,7 @@
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-navigation a' : '.sv100_sv_content_wrapper article .wp-block-navigation a',
+		is_admin() ? '.editor-styles-wrapper .wp-block-navigation a' : '.wp-block-navigation a',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family')
 		)
