93e168573cfbe0f1b14a50bbb3eb622838e14e63
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:33 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-navigation' : '.wp-block-navigation',
+		'.wp-block-navigation',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
@@ -9,7 +9,7 @@
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-navigation .wp-block-navigation__container' : '.wp-block-navigation .wp-block-navigation__container',
+		'.wp-block-navigation .wp-block-navigation__container',
 		array_merge(
 			$module->get_setting('gap')->get_css_data('gap','','px')
 		)
--- lib/css/config/navigation_item.php
+++ lib/css/config/navigation_item.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-navigation .wp-block-navigation__container > .wp-block-navigation-link' : '.wp-block-navigation .wp-block-navigation__container > .wp-block-navigation-link',
+		'.wp-block-navigation .wp-block-navigation__container > .wp-block-navigation-link',
 		array_merge(
 			$module->get_setting('navigation_item_font')->get_css_data('font-family'),
 			$module->get_setting('navigation_item_font_size')->get_css_data('font-size','','px'),
