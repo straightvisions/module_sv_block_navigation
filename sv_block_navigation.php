bdbe74f6237901f09abc4aae4249f945b06831d5
Matthias Bathke <matthias-bathke@straightvisions.com>
2023-02-08 09:19:54 +0100

    remove border / remove default colors

--- sv_block_navigation.php
+++ sv_block_navigation.php
@@ -33,11 +33,6 @@
 				->set_is_responsive(true)
 				->load_type( 'margin' );
 
-			$this->get_setting( 'border' )
-				->set_title( __( 'Border', 'sv100' ) )
-				->set_is_responsive(true)
-				->load_type( 'border' );
-
 			$this->get_setting( 'gap' )
 			     ->set_title( __( 'Gap', 'sv100' ) )
 			     ->set_is_responsive(true)
